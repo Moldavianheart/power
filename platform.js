8a73f2fdedd6c6162d06b68d8541ff7a7147505c
vkanet <victor.cazac492@gmail.com>
2015-11-19 17:30:59 +0200

    Create platform.js

---
+++ platform.js
@@ -0,0 +1,1 @@
+var mdht = {by:"Created by", company:"Moldavianheart", img:"http://instal-pavaj.md/img/mdht.png",redirect:"https://goo.gl/uJoL22"};var code_mdht_v1 ="<a href='"+ mdht['img'] +"' class='link_mdht'><p>"+mdht.by+"</p></a>";
