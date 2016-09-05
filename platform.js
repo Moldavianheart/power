8e94b61d69e8614f38645b87227bd0733c7c4cc6
vkanet <victor.cazac492@gmail.com>
2016-09-05 15:57:49 +0300

    Update platform.js

--- platform.js
+++ platform.js
@@ -1,2 +1,20 @@
-var mdht = {by:"Created by", company:"Moldavianheart", img:"http://instal-pavaj.md/img/mdht.png",redirect:"https://goo.gl/uJoL22"};
-var code_mdht_v1 ="<a href='"+ mdht['img'] +"' class='link_mdht'><p>"+mdht.by+"</p></a>";
+var mdht = {
+	by:"Created by", 
+	company:"Moldavianheart", 
+	img:"http://i.mdhtcdn.net",
+	redirect:"https://goo.gl/uJoL22",
+	positon:function (pm) {
+		if (pm == "center") {
+			//code
+		}else if(pm == "left"){
+			//code
+		}else if(pm == "right"){
+			//code
+		}
+		
+	},
+	size_img:function(sz) {
+		return "<img src='"+this.img+"' width='"+sz+"'>";
+	}
+
+};
